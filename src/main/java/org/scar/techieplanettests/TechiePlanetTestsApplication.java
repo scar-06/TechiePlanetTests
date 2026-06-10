169b8bc8cd876520f5531999b1b44d85b3a08e51
Oscar Nwanze <nwanzeoscar@gmail.com>
2026-06-10 12:02:16 +0100

    initial commit

---
+++ src/main/java/org/scar/techieplanettests/TechiePlanetTestsApplication.java
@@ -0,0 +1,13 @@
+package org.scar.techieplanettests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechiePlanetTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechiePlanetTestsApplication.class, args);
+    }
+
+}
